d8abb1453137dd207aa172ed6285fc686d0c7e7f
Mishanya2222 <146630550+Mishanya2222@users.noreply.github.com>
2025-02-08 11:08:24 +0700

    Update kubick.py

--- kubick.py
+++ kubick.py
@@ -7,12 +7,38 @@
 # Визуализируйте результаты в виде гистограммы.
 import random
 import matplotlib.pyplot
+lst3=[]
+lst2  = []
 lst =[[],[],[],[],[],[]]
 for i in range(1,1000):
     t = random.randint(1,6)
     lst[t-1].append(t)
+    lst2.append(t)
 val = [len(lst[0]),len(lst[1]),len(lst[2]),len(lst[3]), len(lst[4]), len(lst[5])]
 categ = ["1","2","3","4","5","6"]
 
+
+max = 0
+lengt = 0
+for j in lst2:
+    if j == max:
+        lengt = lengt + 1
+
+    else:
+        lst3.append(lengt)
+        max = j
+        lengt=1
+
+
+maxmax = 0
+for r in lst3:
+    if maxmax < r:
+        maxmax = r
+print(maxmax)
+
 matplotlib.pyplot.bar(categ, val)
 matplotlib.pyplot.show()
+
+
+
+#вероятность выпадения каждого значения 1/6
